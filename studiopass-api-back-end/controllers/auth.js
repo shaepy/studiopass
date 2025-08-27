09541f6f6579326bca4df81289b5792e9719f4f1
shaexintran <shaexin@icloud.com>
2025-08-26 21:19:10 -0700

    consolidated auth endpoints to single /sign-up

--- studiopass-api-back-end/controllers/auth.js
+++ studiopass-api-back-end/controllers/auth.js
@@ -4,112 +4,24 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-// /sign-up?role=staff
+// ex. /sign-up?role=student
 router.post("/sign-up", async (req, res) => {
   try {
-    const { role } = req.query;
-    // role = 'admin', 'staff', 'client'
-    // equals to = 'owner', 'instructor', 'student'
+    const { role } = req.query; // role = 'owner', 'instructor', 'student'
+    if (!role) {
+      return res.status(400).json({ err: "Missing role from request query." });
+    }
 
     const userInDatabase = await User.findOne({ username: req.body.username });
     if (userInDatabase) {
       return res.status(409).json({ err: "Username already taken." });
     }
 
-    // create admin account
     const user = await User.create({
       username: req.body.username,
       hashedPassword: bcrypt.hashSync(req.body.password, 12),
       email: req.body.email,
-      role: "owner",
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-    });
-
-    const payload = {
-      username: user.username,
-      _id: user._id,
-      role: user.role,
-    };
-    const token = jwt.sign({ payload }, process.env.JWT_SECRET);
-    res.status(201).json({ user, token });
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
-router.post("/admin/sign-up", async (req, res) => {
-  try {
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (userInDatabase) {
-      return res.status(409).json({ err: "Username already taken." });
-    }
-
-    // create admin account
-    const user = await User.create({
-      username: req.body.username,
-      hashedPassword: bcrypt.hashSync(req.body.password, 12),
-      email: req.body.email,
-      role: "owner",
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-    });
-
-    const payload = {
-      username: user.username,
-      _id: user._id,
-      role: user.role,
-    };
-    const token = jwt.sign({ payload }, process.env.JWT_SECRET);
-    res.status(201).json({ user, token });
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
-router.post("/instructor/sign-up", async (req, res) => {
-  try {
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (userInDatabase) {
-      return res.status(409).json({ err: "Username already taken." });
-    }
-
-    // create instructor account
-    const user = await User.create({
-      username: req.body.username,
-      hashedPassword: bcrypt.hashSync(req.body.password, 12),
-      email: req.body.email,
-      role: "instructor",
-      bio: req.body.bio,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-    });
-
-    const payload = {
-      username: user.username,
-      _id: user._id,
-      role: user.role,
-    };
-    const token = jwt.sign({ payload }, process.env.JWT_SECRET);
-    res.status(201).json({ user, token });
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
-router.post("/student/sign-up", async (req, res) => {
-  try {
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (userInDatabase) {
-      return res.status(409).json({ err: "Username already taken." });
-    }
-
-    // create student account
-    const user = await User.create({
-      username: req.body.username,
-      hashedPassword: bcrypt.hashSync(req.body.password, 12),
-      email: req.body.email,
-      role: "student",
+      role: role,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
     });
@@ -132,7 +44,6 @@ router.post("/sign-in", async (req, res) => {
     console.log("Signing in user:", user);
     if (!user) return res.status(401).json({ err: "Invalid credentials." });
 
-    // compare passwords
     const isPasswordCorrect = bcrypt.compareSync(
       req.body.password,
       user.hashedPassword
@@ -141,7 +52,6 @@ router.post("/sign-in", async (req, res) => {
     if (!isPasswordCorrect)
       return res.status(401).json({ err: "Invalid credentials." });
 
-    // create payload
     const payload = {
       username: user.username,
       _id: user._id,
